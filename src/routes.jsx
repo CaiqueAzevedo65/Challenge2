c60044dbfd057bc3c4a6d80188f54443b9637bfe
Caique Azevedo <caiqueazevedo2005@gmail.com>
2025-05-16 21:22:49 -0300

    [Caique - Troca do nome das páginas]

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Results from "./pages/Results";
-import Project from "./pages/Project";
-import Configurations from "./pages/Configurations";
-import Historico from "./pages/Histórico";
+import Results from "./pages/Results/Results";
+import Project from "./pages/Project/Project";
+import Configurations from "./pages/Configurations/Configurations";
+import Historico from "./pages/Historico/Historico";
 
 
 function AppRoutes() {

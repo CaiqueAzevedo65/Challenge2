928155ec9732493048d3dcab87b59b87f47326f8
Furtadev <gfurtadodev@gmail.com>
2025-05-17 22:29:09 -0300

    Add files via upload
    Gabriel - Mudança

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import "./footer.css"
+
+function Footer() {
+    return (
+        <footer className="d-flex footer justify-content-center align-items-center">
+            <span>&copy; 2025 Prevent. Todos os direitos reservados.</span>
+        </footer>
+    );
+}
+
+export default Footer
